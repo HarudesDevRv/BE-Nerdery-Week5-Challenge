68e332ac8d955ab05f98d4fb3a672f5513bbb6d8
HarudesDevRv <luisrendon@ravn.co>
2026-01-29 13:06:42 -0500

    Feat: Database seeder

---
+++ prisma/seed.ts
@@ -0,0 +1,52 @@
+import prisma from "../src/prisma";
+import * as bcrypt from "bcrypt";
+const saltRounds = 10;
+
+async function main() {
+  await prisma.user.create({
+    include: { address: {} },
+    data: {
+      email: "test_client@mail.com",
+      firstName: "Tester",
+      lastName: "Testing",
+      password: await bcrypt.hash("abc123", saltRounds),
+      address: {
+        create: {
+          address: "Fake street 123",
+          city: "Arequipa",
+          country: "Peru",
+        },
+      },
+    },
+  });
+  await prisma.user.create({
+    include: { address: {} },
+    data: {
+      email: "test_client2@mail.com",
+      firstName: "Tester",
+      lastName: "Testing",
+      password: await bcrypt.hash("abc123", saltRounds),
+      address: {
+        create: {},
+      },
+    },
+  });
+  await prisma.user.create({
+    include: { address: {} },
+    data: {
+      email: "test_manager@mail.com",
+      firstName: "Tester",
+      lastName: "Testing",
+      password: await bcrypt.hash("abc123", saltRounds),
+      address: {
+        create: {},
+      },
+    },
+  });
+}
+
+main().catch(async (e) => {
+  console.error(e);
+  await prisma.$disconnect();
+  process.exit(1);
+});
